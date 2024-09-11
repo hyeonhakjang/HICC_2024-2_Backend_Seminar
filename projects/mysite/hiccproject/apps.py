df1c1dfb6722355253b4dac8446271bd23158375
Jang hyunhak <gusgkr785@gmail.com>
2024-09-12 04:02:50 +0900

    django 세미나

---
+++ projects/mysite/hiccproject/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HiccpraticeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hiccpratice'
---
+++ projects/mysite/hiccproject/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views # .은 해당 폴더 (hiccproject) 의미
+
+urlpatterns = [
+	path('', views.index),
+	# config 폴더의 path(‘home/’, include(‘hiccproject.urls’)), 코드와 합쳐서 	http://127.0.0.1:8000/home/ 경로가 됨
+]
